955a3ceee4261d0ca03a5bc11866d48ec24a1026
Rafael21-boob <29881@raphaeldisanto.com.br>
2026-06-01 12:32:46 -0300

    Add files via upload

--- app/Main.java
+++ app/Main.java
@@ -1,5 +1,38 @@
 package app;
 
+import model.Cat;
+import enums.FurColor;
+
 public class Main {
-    
+
+    public static void main(String[] args) {
+
+        Cat cat = new Cat(
+                "Leon",
+                3,
+                11,
+                FurColor.GRAY,
+                true,
+                "Peixe",
+                25,
+                true
+        );
+
+        cat.displayInfo();
+        System.out.println();
+
+        cat.makeSound();
+        cat.sleep();
+        cat.purr();
+
+        System.out.println();
+
+        cat.move(5, 20);
+        cat.move(3, "rato");
+
+        System.out.println();
+
+        cat.loseLife();
+        cat.loseLife();
+    }
 }
